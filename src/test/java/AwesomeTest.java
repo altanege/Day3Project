ceda4951f4bab5e0771508ec0af17703b03df763
altanege <altanege.1@gmail.com>
2020-10-15 08:44:10 +0200

    Added more Awesome staff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,5 +3,10 @@ public class AwesomeTest {
     // green means it's under stating area
     // grey in .idea folder means it's ignored
     // when the color is normal, it means it's commited no changes
+
+    // I just pulled the new change from GitHub that I did not have locally
+    // and now my project is in sync with repository
+
+    // Now i made more awesome changes and I am about to push this back to Github
 }
 
